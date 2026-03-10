a373c8e487d23041316e67595261ea404b26deb5
Adolfo Usier <adolfousier@gmail.com>
2026-03-10 09:04:06 +0000

    docs: sync testing page

--- docs/book/searcher-c2a407aa.js
+++ docs/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5a4ffe2c.js',
+                path_to_root + 'searchindex-20b146fc.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
