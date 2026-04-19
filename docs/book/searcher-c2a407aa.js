90bbbe7097e96332e429bcf7a6d2c8bf14558d11
Adolfo Usier <adolfousier@gmail.com>
2026-04-19 18:11:49 +0100

    v0.1.5: sync docs v0.3.12, update testing.md to 2,062 tests

--- docs/book/searcher-c2a407aa.js
+++ docs/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d2a8861f.js',
+                path_to_root + 'searchindex-ede15e79.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
