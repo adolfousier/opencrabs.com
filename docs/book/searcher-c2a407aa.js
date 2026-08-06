efb1d5504338f9e52945c85c2e47d2a3cdaed1f1
Adolfo Usier <adolfousier@gmail.com>
2026-08-06 18:50:08 +0100

    docs: rebuild book for v0.3.79 content

--- docs/book/searcher-c2a407aa.js
+++ docs/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-6037411d.js',
+                path_to_root + 'searchindex-35d927d9.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
