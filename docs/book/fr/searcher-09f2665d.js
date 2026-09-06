3fb03efd74b2997a91a668765af83953faf2f7c0
Adolfo Usier <adolfousier@gmail.com>
2026-09-06 21:14:57 +0100

    docs(book): rebuild for the root troubleshooting entry, two user stories, and the SVG favicon
    English plus pt-PT, es, fr, ru, id via build-all.sh.

--- docs/book/fr/searcher-09f2665d.js
+++ docs/book/fr/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0613653c.js',
+                path_to_root + 'searchindex-341096e7.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
