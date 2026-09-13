400191121dcb0f0eeff6f78f3810b8e942c90346
Adolfo Usier <adolfousier@gmail.com>
2026-09-13 14:51:52 +0100

    docs(book): rebuild, all locales

--- docs/book/es/searcher-09f2665d.js
+++ docs/book/es/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-493a2404.js',
+                path_to_root + 'searchindex-fba88e75.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
